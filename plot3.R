e12a1a3c7c2d7057fc6247840464a620f5878b31
zhanna-ovcharenko <zhanna.ovcharenko@gmail.com>
2014-10-13 01:07:09 +0300

    Added script4/plot4. Updated legend for plot3

--- plot3.R
+++ plot3.R
@@ -18,7 +18,7 @@ energy <- read.table(file = "household_power_consumption.txt", header = TRUE, se
 with(energy, plot(DateTime, as.numeric(as.character(Sub_metering_1)), type = "l",  xlab = "", ylab = "Energy sub metering"))
 with(energy, points(DateTime, as.numeric(as.character(Sub_metering_2)), type = "l", col = "red"))
 with(energy, points(DateTime, as.numeric(as.character(Sub_metering_3)), type = "l", col = "blue"))
-legend("topright", lwd = 1, col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.8, bty = "o")
+legend("topright", lwd = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "o", cex = 0.8, x.intersp=0.5, y.intersp=0.5)
 
 ##
 ##Save to PNG file
